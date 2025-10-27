dce28bdfddb23f89ed442b2675ad5a6be522a6f1
amurseli <agustinmurseli@gmail.com>
2025-10-27 09:53:47 -0300

    Back y Front funcionales y Dockerizados

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,19 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Campaign, Donation
-
+from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     list_display = ['username', 'email', 'is_staff', 'created_at']
     list_filter = ['is_staff', 'is_superuser', 'is_active']
 
-@admin.register(Campaign)
-class CampaignAdmin(admin.ModelAdmin):
-    list_display = ['title', 'creator', 'goal', 'created_at']
-    list_filter = ['created_at']
-    search_fields = ['title', 'description']
-
-@admin.register(Donation)
-class DonationAdmin(admin.ModelAdmin):
-    list_display = ['donor', 'campaign', 'amount', 'created_at']
-    list_filter = ['created_at']
