754260b20ffdde9a3bebe1ab5a92c149897654e4
amurseli <agustinmurseli@gmail.com>
2025-11-02 12:46:39 -0300

    Mudanza a Spring

--- backend/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'email', 'is_staff', 'created_at']
-    list_filter = ['is_staff', 'is_superuser', 'is_active']
-
---
+++ backend/src/main/java/com/hornero/HorneroApplication.java
@@ -0,0 +1,12 @@
+package com.hornero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorneroApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(HorneroApplication.class, args);
+    }
+}
