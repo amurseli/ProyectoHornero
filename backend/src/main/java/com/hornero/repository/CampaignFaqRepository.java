e8cd3ccce93e72ff96abf9cfc1bac4bfdebd1e42
Agustín Murseli <agustinmurseli@gmail.com>
2026-05-17 17:25:52 -0300

    Fix

--- backend/src/main/java/com/hornero/repository/CampaignFaqRepository.java
+++ backend/src/main/java/com/hornero/repository/CampaignFaqRepository.java
@@ -1,12 +1,12 @@
 package com.hornero.repository;
 
-import com.hornero.model.Reward;
+import com.hornero.model.CampaignFaq;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface RewardRepository extends JpaRepository<Reward, Long> {
-    List<Reward> findByCampaignIdOrderByDisplayOrderAsc(Long campaignId);
+public interface CampaignFaqRepository extends JpaRepository<CampaignFaq, Long> {
+    List<CampaignFaq> findByCampaignIdOrderByDisplayOrderAsc(Long campaignId);
     void deleteByCampaignId(Long campaignId);
 }
