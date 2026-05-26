63799f01ed374f597b3228ef982c14122959dc05
Agustín Murseli <agustinmurseli@gmail.com>
2026-05-26 01:58:29 -0300

    re:fix migración 22

--- backend/src/main/resources/db/migration/V22__add_reward_price_to_contribution.sql
+++ backend/src/main/resources/db/migration/V22__add_reward_price_to_contribution.sql
@@ -1,7 +1,3 @@
--- En lugar de esto:
-ALTER TABLE payments.contribution
-    ADD COLUMN reward_price NUMERIC(15, 2);
 
--- Usar esto:
 ALTER TABLE payments.contribution
     ADD COLUMN IF NOT EXISTS reward_price NUMERIC(15, 2);
